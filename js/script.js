d0bfecf4e0a33283f236c359de49d71a544de9b4
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2022-10-13 14:53:51 +0200

    add box elements with style according to number

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
+
+
+
+
 for (let i=1; i <= 100; i++) {
+  const container = document.querySelector('.container')
+  const box = document.createElement('div');
+  box.className = 'box';
+  container.append(box);
   if (!(i % 3) && !(i % 5)) {
-    console.log("FizzBuzz")
+    box.innerHTML = "FizzBuzz";
+    box.classList.add('fizzbuzz-number')
   } else if (!(i % 5)) {
-    console.log("Buzz")
+    box.innerHTML = "Buzz";
+    box.classList.add('buzz-number')
   } else if (!(i % 3)) {
-    console.log("Fizz")
+    box.innerHTML = "Fizz";
+    box.classList.add('fizz-number')
   } else {
-    console.log(i)
+    box.innerHTML = i;
+    box.classList.add('just-number')
   }
 }
